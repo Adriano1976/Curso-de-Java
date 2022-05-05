9c82e068ba5f597baec4022283694d7fd746b4b4
Adriano Santos <adriradiologista@hotmail.com.br>
2022-05-05 16:18:36 -0300

    Criando classes Account, Account2Text com variável do tipo private usando um construtor.

---
+++ 02 - Introdução de Classes, Objetos e Métodos/class-object-method/src/Accounts.java
@@ -0,0 +1,17 @@
+public class Accounts {
+
+    // método para definir o nome do objeto
+    private String name; // variável de instância
+
+    // método para definir o nome do objeto
+    public void setName(String name) {
+
+        this.name = name; // armazena o nome
+    }
+
+    // método para recuperar o nome do objeto.
+    public String getName(){
+
+        return name; // retorna valor do nome para o chamador
+    }
+} // fim da classe Account
