ebf8a7a5df56a8579cce90b110c444f53cbb0a14
Adriano Santos <adriradiologista@hotmail.com.br>
2022-07-07 13:03:35 -0300

    Program Contract with interface.

---
+++ 10-Interfaces/withInterface/src/model/services/RentalService.java
@@ -0,0 +1,41 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private TaxService taxService;
+
+    public RentalService() {
+    }
+
+    public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        // getTime retorna as horas em milesegundos.
+        long timeStart = carRental.getStart().getTime();
+        long timeFinish = carRental.getFinish().getTime();
+
+        double hours = (double) (timeFinish - timeStart) / 1000 / 60 / 60;
+
+        double basicPayment;
+
+        if ( hours <= 10.0) {
+            basicPayment = Math.ceil(hours) * pricePerHour;
+        } else {
+            basicPayment = Math.ceil(hours / 24) * pricePerDay;
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
