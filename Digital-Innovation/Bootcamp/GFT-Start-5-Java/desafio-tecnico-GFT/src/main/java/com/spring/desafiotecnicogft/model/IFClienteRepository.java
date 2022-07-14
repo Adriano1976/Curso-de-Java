5b8a1caee7f5400c0f44248760dc064301245ae6
Adriano Santos <adriradiologista@hotmail.com.br>
2022-07-14 11:15:44 -0300

    Update files.

--- Digital-Innovation/Bootcamp/GFT-Start-5-Java/desafio-tecnico-GFT/src/main/java/com/spring/desafiotecnicogft/model/IFClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.spring.desafiotecnicogft.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IFClienteRepository extends CrudRepository<Cliente, Long> {
-
-}
--- Digital-Innovation/Bootcamp/GFT-Start-5-Java/desafio-tecnico-GFT/src/main/java/com/spring/desafiotecnicogft/model/IFTelefoneRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.spring.desafiotecnicogft.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IFTelefoneRepository extends CrudRepository<Telefone, String> {
-}
