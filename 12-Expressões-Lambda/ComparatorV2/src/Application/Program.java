0cdb873615919b6c5b99ec168d7cbf56cf69de07
Adriano Santos <adriradiologista@hotmail.com.br>
2022-07-16 22:43:44 -0300

    Comparator objeto de expressão lambda sem chaves.

---
+++ 12-Expressões-Lambda/ComparatorV2/src/Application/Program.java
@@ -0,0 +1,30 @@
+package Application;
+
+import Entities.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV Sansung", 900.00));
+        list.add(new Product("Notebook LG", 1200.00));
+        list.add(new Product("Tablet Lenovo", 450.00));
+        list.add(new Product("Computador Clone", 1250.00));
+        list.add(new Product("Geladeira Brastemp", 1450.00));
+        list.add(new Product("Iphone 8 20G", 1280.00));
+
+        // Comparator objeto de expressão lambda sem chaves
+        Comparator<Product> comparator = Comparator.comparing(p -> p.getName().toUpperCase());
+        list.sort(comparator);
+
+        for (Product product : list) {
+            System.out.println(product);
+        }
+    }
+}
