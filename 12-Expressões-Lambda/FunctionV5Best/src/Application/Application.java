403b2f37d783afaa916a7762a5c7f7e3823f0db4
Adriano Santos <adriradiologista@hotmail.com.br>
2022-07-19 15:35:00 -0300

    5.ª Version: Expressão lambda inline.

---
+++ 12-Expressões-Lambda/FunctionV5Best/src/Application/Application.java
@@ -0,0 +1,31 @@
+package Application;
+
+import Entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV Sansung", 900.00));
+        list.add(new Product("Notebook LG", 1200.00));
+        list.add(new Product("Tablet Lenovo", 450.00));
+        list.add(new Product("Computador Clone", 1250.00));
+        list.add(new Product("Geladeira Brastemp", 1450.00));
+        list.add(new Product("Iphone 8 20G", 1280.00));
+        list.add(new Product("Secador de Cabela", 89.00));
+        list.add(new Product("Fritadeira Amvox", 59.00));
+
+        // 5.ª Version: Expressão lambda inline.
+        List<String> names = list.stream().map(product -> product.getName().toUpperCase()).toList();
+
+        names.forEach(System.out::println);
+    }
+}
