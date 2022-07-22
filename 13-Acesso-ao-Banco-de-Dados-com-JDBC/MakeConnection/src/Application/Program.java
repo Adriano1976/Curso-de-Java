11827123272fc5c133259fc449b7cda69d3603c7
Adriano Santos <adriradiologista@hotmail.com.br>
2022-07-22 15:36:42 -0300

    Make database connections with "Connection, Properties and DriverManager"

---
+++ 13-Acesso-ao-Banco-de-Dados-com-JDBC/MakeConnection/src/Application/Program.java
@@ -0,0 +1,14 @@
+package Application;
+
+import db.DB;
+
+import java.sql.Connection;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Connection connection = DB.getConnection();
+        DB.closeConnection();
+    }
+}
