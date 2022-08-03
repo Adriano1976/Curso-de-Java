43da8f7206e77928342f0327f1f6279133449bc5
Adriano Santos <adriradiologista@hotmail.com.br>
2022-08-03 15:15:13 -0300

    Create Main view controller

---
+++ Aplication/src/main/java/gui/application/workshop/MainViewController.java
@@ -0,0 +1,80 @@
+package gui.application.workshop;
+
+import gui.application.workshop.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
+import model.services.SellerService;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        loadView("SellerList.fxml", (SellerListController controller) -> {
+            controller.setSellerService(new SellerService());
+            controller.updateTableView();
+        });
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        loadView("DepartmentList.fxml", (DepartmentListController controller) -> {
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        });
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        loadView("About.fxml", action -> {
+        });
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVbox = fxmlLoader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVbox.getChildren());
+
+            T controller = fxmlLoader.getController();
+            initializingAction.accept(controller);
+
+        } catch (IOException exception) {
+            Alerts.showAlert("IO Excepton: ", "There was an error loading the page view!", exception.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+}
