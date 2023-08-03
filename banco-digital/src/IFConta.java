a21631399b057ae6ff8780c19beef3d81d630c78
Adriano Santos <adriradiologista@hotmail.com.br>
2023-08-03 01:33:48 -0300

    Create IFConta.java

---
+++ banco-digital/src/IFConta.java
@@ -0,0 +1,12 @@
+public interface IFConta {
+
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor, IFConta contaDestino);
+
+    void imprimirSaldo();
+    
+    void imprimirExtrato();
+}
